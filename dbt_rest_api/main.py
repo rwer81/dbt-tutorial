0ec010574587ce9f59842c08b670ff302a4b90ad
rwer81 <mshin2781@gmail.com>
2023-04-30 15:38:15 +0100

    initial commit

--- dbt_rest_api/main.py
+++ dbt_rest_api/main.py
@@ -1,8 +1,11 @@
 import os
+import queue
+import threading
+import subprocess
 import logging
+import traceback
 import google.cloud.logging
 from flask import Flask, request
-from process_commands import run_dbt_commands
 
 
 app = Flask(__name__)
@@ -11,24 +14,54 @@ client = google.cloud.logging.Client()
 client.setup_logging()
 
 
+def run_dbt_command():
+    with lock:
+        while not cmd_queue.empty():
+            try:
+                dbt_command = cmd_queue.get()
+                print(dbt_command)
+                dbt_work_dir = os.environ.get("DBT_DIR")
+                print(dbt_work_dir)
+                process = subprocess.Popen(dbt_command, cwd=dbt_work_dir,
+                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                           text=True)
+                process.wait()
+                out_log = process.stdout.read()
+                err_log = process.stderr.read()
+
+                logging.info(out_log)
+                logging.info(err_log)
+                print("completed")
+                print(out_log)
+                print(err_log)
+            except Exception:
+                return str(traceback.print_exc())
+
+
 @app.route("/get_command", methods=['POST'])
-def get_command():
-
+def submit_job():
     srv_json_data = request.get_json()
-    logging.info(srv_json_data)
+    if "command" not in srv_json_data.keys():
+        return "'command' key not found"
+    requested_command = srv_json_data["command"]
 
-    try:
-        requested_command = srv_json_data["command"]
-    except KeyError:
-        return "<command> key not found"
+    if not requested_command.startswith("dbt "):
+        return "Not a DBT command"
 
-    command_run_result = run_dbt_commands(requested_command)
-    logging.info(command_run_result)
-
-    return command_run_result
+    # more checks for commands
+    dbt_command = requested_command.strip()
+    dbt_command = dbt_command.split(" ")
+    # more checks for commands
+    if not cmd_queue.empty():
+        cmd_queue.put(dbt_command)
+        return 'QUEUED. New queue size {0}'.format(cmd_queue.qsize()), 200
+    else:
+        cmd_queue.put(dbt_command)
+        cc = threading.Thread(target=run_dbt_command)
+        cc.start()
+        return 'QUEUED and STARTED. New queue size {0}'.format(cmd_queue.qsize()), 200
 
 # service document
-
 
 @app.route("/")
 def welcome_page():
@@ -36,4 +69,6 @@ def welcome_page():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get("HOST", "0.0.0.0"), port=int(os.environ.get("PORT", 8080)))
+    cmd_queue = queue.Queue()
+    lock = threading.RLock()
+    app.run(host=os.environ.get("HOST", "0.0.0.0"), port=int(os.environ.get("PORT", 8080)), threaded=False)
