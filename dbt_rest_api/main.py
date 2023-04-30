6cd3bf35dfd945053506b32886d80c9491b1cbfb
rwer81 <mshin2781@gmail.com>
2023-04-30 19:28:48 +0100

    initial commit

--- dbt_rest_api/main.py
+++ dbt_rest_api/main.py
@@ -11,25 +11,6 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-def run_dbt_command():
-    logging.info("in run_dbt_command")
-    with lock:
-        while not cmd_queue.empty():
-            try:
-
-                dbt_command = cmd_queue.get()
-                logging.info(dbt_command)
-                dbt_work_dir = os.environ.get("DBT_DIR")
-                logging.info(dbt_work_dir)
-                process = subprocess.Popen(dbt_command, cwd=dbt_work_dir,
-                                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-
-                logging.info(process.stdout.read())
-                logging.info("completed")
-            except Exception:
-                logging.info(str(traceback.print_exc()))
-
-
 @app.route("/get_command", methods=['POST'])
 def submit_job():
     logging.info("in submit_job")
@@ -40,19 +21,22 @@ def submit_job():
 
     if not requested_command.startswith("dbt "):
         return "Not a DBT command"
-
     # more checks for commands
     dbt_command = requested_command.strip()
     dbt_command = dbt_command.split(" ")
-    # more checks for commands
-    if not cmd_queue.empty():
-        cmd_queue.put(dbt_command)
-        return 'QUEUED. New queue size {0}'.format(cmd_queue.qsize()), 200
-    else:
-        cmd_queue.put(dbt_command)
-        cc = threading.Thread(target=run_dbt_command)
-        cc.start()
-        return 'QUEUED and STARTED. New queue size {0}'.format(cmd_queue.qsize()), 200
+
+    try:
+        dbt_work_dir = os.environ.get("DBT_DIR")
+        logging.info(dbt_work_dir)
+        process = subprocess.Popen(dbt_command, cwd=dbt_work_dir,
+                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        process.wait()
+        return process.stdout.read()
+
+    except Exception:
+        return str(traceback.print_exc())
+
+
 
 # service document
 
@@ -64,7 +48,7 @@ def welcome_page():
 if __name__ == '__main__':
     client = google.cloud.logging.Client()
     client.setup_logging()
+
     logging.info("in maninnnnnnnnnnnnnn")
-    cmd_queue = queue.Queue()
-    lock = threading.RLock()
+
     app.run(host=os.environ.get("HOST", "0.0.0.0"), port=int(os.environ.get("PORT", 8080)), threaded=False)
