ee1968ceaa8fd7b5cbd7c8e9ef4fba95ad69a297
rwer81 <mshin2781@gmail.com>
2023-04-30 21:15:06 +0100

    initial commit

--- dbt_rest_api/main.py
+++ dbt_rest_api/main.py
@@ -1,54 +1,68 @@
 import os
-import queue
-import threading
 import subprocess
-import logging
-import traceback
-import google.cloud.logging
-from flask import Flask, request
-
+import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/get_command", methods=['POST'])
+@app.route("/run_command", methods=['POST'])
 def submit_job():
-    logging.info("in submit_job")
-    srv_json_data = request.get_json()
-    if "command" not in srv_json_data.keys():
-        return "'command' key not found"
-    requested_command = srv_json_data["command"]
 
-    if not requested_command.startswith("dbt "):
-        return "Not a DBT command"
-    # more checks for commands
-    dbt_command = requested_command.strip()
-    dbt_command = dbt_command.split(" ")
+    service_json_data = request.get_json()
 
-    try:
+    validated_data = validate_command(service_json_data)
+    if isinstance(validated_data, list):
         dbt_work_dir = os.environ.get("DBT_DIR")
-        logging.info(dbt_work_dir)
-        process = subprocess.Popen(dbt_command, cwd=dbt_work_dir,
-                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+        process = subprocess.Popen(validated_data, cwd=dbt_work_dir,
+                                       stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         process.wait()
-        return process.stdout.read()
 
-    except Exception:
-        return str(traceback.print_exc())
-
+        return jsonify("Command executed successfully. Result: " + process.stdout.read()), 200
+    else:
+        return jsonify({"code": 400, "name": "BadRequest", "description": validated_data})
 
 
 # service document
 
 @app.route("/")
 def welcome_page():
-    return "Welcome"
+    return jsonify("Welcome")
+
+
+def validate_command(json_data):
+
+    requested_command = json_data["command"]
+    requested_command = requested_command.strip()
+
+    if "command" not in json_data.keys() or len(json_data.keys()) != 1 or not isinstance(requested_command, str):
+        response = '"command" key not found or misconfigured request. '\
+                    'Example usage: {"command": "dbt test"}'
+
+    elif any(multi_cmd in requested_command for multi_cmd in ["&&", ";", "||"]):
+        response = "Multiple commands not allowed"
+
+    elif not requested_command.startswith("dbt "):
+        response = "Not a DBT command"
+    else:
+        response = requested_command.split(" ")
+
+    return response
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 
 if __name__ == '__main__':
-    client = google.cloud.logging.Client()
-    client.setup_logging()
-
-    logging.info("in maninnnnnnnnnnnnnn")
 
     app.run(host=os.environ.get("HOST", "0.0.0.0"), port=int(os.environ.get("PORT", 8080)), threaded=False)
