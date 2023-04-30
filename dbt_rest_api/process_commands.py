b1b94636ee2357b1ac5a0b8d1db4376d7238a6a2
rwer81 <mshin2781@gmail.com>
2023-04-30 13:12:57 +0100

    initial commit

--- dbt_rest_api/process_commands.py
+++ dbt_rest_api/process_commands.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import traceback
 
@@ -9,11 +10,9 @@ def run_dbt_commands(dbt_command):
         dbt_command = dbt_command.strip()
         # more checks for commands
 
-        sub_command = ["python", "run_commands.py", dbt_command]
+        sub_command = "python run_commands.py {0} &".format(dbt_command)
 
-        process = subprocess.Popen(sub_command,
-                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                   text=True, close_fds=True)
+        process = os.system(sub_command)
 
         return "Command started."
 
